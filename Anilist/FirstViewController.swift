0dadbcd7f29b6a75d9517f8a72bc125c07a95253
NaotoIto <naotoufu@outlook.com>
2018-10-21 19:00:51 +0900

    Initial Commit

---
+++ Anilist/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Anilist
+//
+//  Created by NaotoIto on 2018/10/21.
+//  Copyright © 2018 NaotoIto. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
